f7632edbbc31fb42bfd71d3ddda55f16cf621552
muhameddadel <muhameddadel2000@gmail.com>
2023-05-25 19:42:04 +0300

    login setup

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'muhamedadel'
-    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -20,11 +20,19 @@ def create_app():
     
     from .models import User
 
-    cerate_database(app)
+    with app.app_context():
+        create_database()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
     
-
-def cerate_database(app):
-    if not path.exists('website/'+DB_NAME):
-        db.create_all(app=app)
+def create_database():
+    if not path.exists('website/' + DB_NAME):
+        db.create_all()
