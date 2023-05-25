049d7899a8569475daac6a85f5395d763097b1ce
muhameddadel <muhameddadel2000@gmail.com>
2023-05-25 19:21:17 +0300

    models

--- website/__init__.py
+++ website/__init__.py
@@ -18,6 +18,8 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')    
     
+    from .models import User
+
     cerate_database(app)
 
     return app
